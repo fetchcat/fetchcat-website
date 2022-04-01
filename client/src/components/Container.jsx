69ba64fe5f4c8ab72da3c2091c67e1c8d736d9af
Michelle Rogers <michelleevarogers@gmail.com>
2022-03-31 21:45:11 -0700

    merge

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -2,22 +2,7 @@ import React from "react";
 
 import styled from "styled-components";
 
-<<<<<<< HEAD
 // --- Component //
-=======
-const StyledContainer = styled.div`
-  background-color: ${(props) => props.theme.lightGrey};
-  display: flex;
-  flex-direction: column;
-  padding-top: 100px;
-  height: 100%;
-  width: 100%;
-  h1 {
-    margin: 25px 0px;
-    text-align: center;
-  }
-`;
->>>>>>> 9840766758854dd694af3c2876335c2814d6d25f
 
 const Container = ({ children }) => {
   return <StyledContainer>{children}</StyledContainer>;
--- client/src/components/Msg/ErrorMsg.jsx
+++ client/src/components/Msg/ErrorMsg.jsx
@@ -1,25 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-<<<<<<< HEAD
 // --- Component --- //
-=======
-const StyledErrorMsg = styled.div`
-  color: red;
-`;
->>>>>>> 9840766758854dd694af3c2876335c2814d6d25f
 
 const ErrorMsg = ({ msg }) => {
   return <StyledErrorMsg>{msg}</StyledErrorMsg>;
 };
 
 export default ErrorMsg;
-<<<<<<< HEAD
 
 // ---  Styles --- //
 
 const StyledErrorMsg = styled.div`
   color: red;
 `;
-=======
->>>>>>> 9840766758854dd694af3c2876335c2814d6d25f
